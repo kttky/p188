550d82055aa8f6093f03e2c3b64813bff35dfda6
Kttkky <84614763+kttky@users.noreply.github.com>
2021-05-29 14:51:26 +0900

    aありがとうさんきゅう

--- p188/ContentView.swift
+++ p188/ContentView.swift
@@ -8,7 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var num : Int = 0
+    
     var body: some View {
+        HStack{
+            //数値の表示
+            Text("\(num)")
+                .font(.system(size:50))
+            //カウントアップ
+            Button(action: {num += 1},label :{
+                    Text("Tap").font(.largeTitle)
+            })
+        }
         Text("Hello, world!")
             .padding()
     }
