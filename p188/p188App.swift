7190e96b76c874136713ed8ff6f341396d1b1b54
Kttkky <84614763+kttky@users.noreply.github.com>
2021-05-29 14:39:52 +0900

    Initial Commit

---
+++ p188/p188App.swift
@@ -0,0 +1,17 @@
+//
+//  p188App.swift
+//  p188
+//
+//  Created by 加月拓弥 on 2021/05/29.
+//
+
+import SwiftUI
+
+@main
+struct p188App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
